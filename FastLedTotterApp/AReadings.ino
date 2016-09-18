fdf061d1a83003eb1247564c9a74598467cead11
Niel McLaren <d@nielmclaren.com>
2016-09-17 17:02:36 -0700

    Moar tilt reading interpretation.

---
+++ FastLedTotterApp/AReadings.ino
@@ -0,0 +1,83 @@
+
+// NOTE: This file is named for alphabetical ordering.
+
+const int maxTilt = 4;
+
+float tilt = 0;
+float tiltDirection = 0;
+
+const int maxTiltReadings = 5;
+int numTiltReadings = 0;
+float tiltReadings[maxTiltReadings];
+
+float prevTiltAverage = 0;
+float tiltAverage = 0;
+int tiltAverageDirection = 0;
+int prevTiltAverageDirection = 0;
+
+int tiltMoveDirection = 0;
+int prevTiltMoveDirection = 0;
+
+bool isSwitching = false;
+int prevSwitchTiltDirection = 0;
+unsigned long prevSwitchTime = 0;
+
+
+void updateTilt() {
+  tilt = constrain(CircuitPlayground.motionX() / maxTilt, -1, 1);
+}
+
+void updateTiltDirection() {
+  if (tilt == 0) {
+    tiltDirection = 0;
+  }
+  else {
+    tiltDirection = abs(tilt) / tilt;
+  }
+}
+
+void updateTiltReadings() {
+  if (numTiltReadings < maxTiltReadings) {
+    tiltReadings[numTiltReadings] = tiltReadings[numTiltReadings - 1];
+    numTiltReadings++;
+  }
+  for (int i = numTiltReadings - 1; i > 0; i--) {
+    tiltReadings[i] = tiltReadings[i - 1];
+  }
+
+  tiltReadings[0] = tilt;
+  if (numTiltReadings <= 0) {
+    numTiltReadings = 1;
+  }
+}
+
+void updateTiltAverage() {
+  prevTiltAverage = tiltAverage;
+  prevTiltAverageDirection = tiltAverageDirection;
+  tiltAverage = 0;
+  for (int i = 0; i < numTiltReadings; i++) {
+    tiltAverage += tiltReadings[i];
+  }
+  tiltAverage /= numTiltReadings;
+  tiltAverageDirection = abs(tiltAverage) / tiltAverage;
+}
+
+void updateTiltMoveDirection() {
+  float tiltMoveDirectionDelta = tiltReadings[0] - tiltReadings[numTiltReadings - 1];
+  if (abs(tiltMoveDirectionDelta) > 0.05) {
+    prevTiltMoveDirection = tiltMoveDirection;
+    tiltMoveDirection = abs(tiltMoveDirectionDelta) / tiltMoveDirectionDelta;
+  }
+}
+
+void updateSwitching() {
+  isSwitching = tiltMoveDirection != prevTiltMoveDirection
+      && tiltDirection != prevSwitchTiltDirection
+      && abs(tilt) > 0.07;
+
+  if (isSwitching) {
+    prevSwitchTiltDirection = tiltDirection;
+    prevSwitchTime = millis();
+  }
+}
+
--- FastLedTotterApp/FastLedTotterApp.ino
+++ FastLedTotterApp/FastLedTotterApp.ino
@@ -8,10 +8,6 @@ CRGB leds[numLedsPerStrip];
 const int globalSaturation = 255;
 const int globalLightness = 64;
 
-const int maxTilt = 4;
-float tilt;
-float tiltDirection;
-
 void setup() {
   Serial.begin(9600);
   Serial.println("Start");
@@ -21,14 +17,16 @@ void setup() {
   FastLED.addLeds<NEOPIXEL, 6>(leds, numLedsPerStrip);
   FastLED.addLeds<NEOPIXEL, 9>(leds, numLedsPerStrip);
 
-  tilt = 0;
-
   setupQuantizeMode();
 }
 
 void loop() {
-  readTilt();
-  readTiltDirection();
+  updateTilt();
+  updateTiltDirection();
+  updateTiltReadings();
+  updateTiltAverage();
+  updateTiltMoveDirection();
+  updateSwitching();
 
   loopQuantizeMode();
 
@@ -36,15 +34,3 @@ void loop() {
   delay(2);
 }
 
-void readTilt() {
-  tilt = constrain(CircuitPlayground.motionX() / maxTilt, -1, 1);
-}
-
-void readTiltDirection() {
-  if (tilt == 0) {
-    tiltDirection = 0;
-  }
-  else {
-    tiltDirection = abs(tilt) / tilt;
-  }
-}
