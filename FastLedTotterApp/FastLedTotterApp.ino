87d53dd48543840df15815530b02c20d04cd1700
Niel McLaren <d@nielmclaren.com>
2016-09-17 21:33:30 -0700

    Added mode navigation.

--- FastLedTotterApp/FastLedTotterApp.ino
+++ FastLedTotterApp/FastLedTotterApp.ino
@@ -8,6 +8,17 @@ CRGB leds[numLedsPerStrip];
 const int globalSaturation = 255;
 const int globalLightness = 64;
 
+bool leftButtonDebouncer = false;
+bool rightButtonDebouncer = false;
+
+int mode = 0;
+
+#define MODE_TEST -1
+#define MODE_IDLE 0
+#define MODE_QUANTIZE 1
+#define MODE_BEAM 2
+#define MODE_LEVEL 3
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Start");
@@ -26,12 +37,64 @@ void loop() {
   updateTiltMoveDirection();
   updateSwitching();
 
-  //loopQuantizeMode();
-  //loopBeamMode();
-  loopLevelMode();
+  updateButtonHandlers();
+
+  loopMode();
 
   FastLED.show();
   delay(2);
+}
+
+void updateButtonHandlers() {
+  if (CircuitPlayground.leftButton()) {
+    if (!leftButtonDebouncer) {
+      leftButtonDebouncer = true;
+      leftButtonHandler();
+    }
+  }
+  else {
+    leftButtonDebouncer = false;
+  }
+
+  if (CircuitPlayground.rightButton()) {
+    if (!rightButtonDebouncer) {
+      rightButtonDebouncer = true;
+      rightButtonHandler();
+    }
+  }
+  else {
+    rightButtonDebouncer = false;
+  }
+}
+
+void leftButtonHandler() {
+  if (--mode < 1) {
+    mode = 3;
+  }
+}
+
+void rightButtonHandler() {
+  if (++mode > 3) {
+    mode = 1;
+  }
+}
+
+void loopMode() {
+  switch (mode) {
+    case MODE_TEST:
+      break;
+    case MODE_IDLE:
+      break;
+    case MODE_QUANTIZE:
+      loopQuantizeMode();
+      break;
+    case MODE_BEAM:
+      loopBeamMode();
+      break;
+    case MODE_LEVEL:
+      loopLevelMode();
+      break;
+  }
 }
 
 float modTime(long period) {
